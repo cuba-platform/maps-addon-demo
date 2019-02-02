1c6e8fc2e9c850714e6670526890c751fed75d3d
Gleb Shalyganov <shalyganov@haulmont.com>
2019-02-02 14:21:08 +0400

    canvas screen were changed according to the api changes

--- modules/web/src/com/company/mapsdemo/web/screens/CanvasDemo.java
+++ modules/web/src/com/company/mapsdemo/web/screens/CanvasDemo.java
@@ -3,8 +3,10 @@ package com.company.mapsdemo.web.screens;
 import com.haulmont.addon.maps.gis.utils.GeometryUtils;
 import com.haulmont.addon.maps.web.gui.components.CanvasLayer;
 import com.haulmont.addon.maps.web.gui.components.GeoMap;
+import com.haulmont.addon.maps.web.gui.components.PopupWindowOptions;
 import com.haulmont.addon.maps.web.gui.components.layer.style.FontPointIcon;
 import com.haulmont.addon.maps.web.gui.components.layer.style.PointStyle;
+import com.haulmont.addon.maps.web.gui.components.layer.style.PolygonStyle;
 import com.haulmont.cuba.gui.components.AbstractWindow;
 import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.icons.CubaIcon;
@@ -12,6 +14,7 @@ import com.vaadin.ui.Notification;
 
 import javax.inject.Inject;
 import java.util.Map;
+import java.util.function.Function;
 
 public class CanvasDemo extends AbstractWindow {
     @Inject
@@ -19,44 +22,35 @@ public class CanvasDemo extends AbstractWindow {
 
     private CanvasLayer canvasLayer;
 
-    @Inject
-    private Button drawPoint;
-    @Inject
-    private Button drawPolygon;
-    @Inject
-    private Button drawPolyline;
-
     @Override
     public void init(Map<String, Object> params) {
         canvasLayer = map.getCanvas();
         map.selectCanvas();
 
         CanvasLayer.Point location = canvasLayer.addPoint(GeometryUtils.createPoint(50, 50));
-        FontPointIcon icon = new FontPointIcon(CubaIcon.CAB)
-                .setIconPathFillColor("#f4d142")
-                .setIconTextFillColor("black")
-                .setIconPathStrokeColor("black");
-
-        location.setStyle(new PointStyle(icon))
+        location.setStyle(new PointStyle(
+                new FontPointIcon(CubaIcon.CAB)
+                        .setIconPathFillColor("#f4d142")
+                        .setIconTextFillColor("black")
+                        .setIconPathStrokeColor("black")))
                 .setPopupContent("Taxi location")
                 .setEditable(true)
                 .addRightClickListener(e -> Notification.show("Right click"));
-
-        canvasLayer.addAfterPointDrawnListener(drawnEvent -> canvasLayer.addPoint(drawnEvent.getPoint()));
-        canvasLayer.addAfterPolylineDrawnListener(drawnEvent -> canvasLayer.addPolyline(drawnEvent.getPolyline()));
-        canvasLayer.addAfterPolygonDrawnListener(drawnEvent -> canvasLayer.addPolygon(drawnEvent.getPolygon()));
     }
 
     public void drawPoint() {
-        canvasLayer.drawPoint();
+        canvasLayer.drawPoint(point -> {
+        });
     }
 
     public void drawPolyline() {
-        canvasLayer.drawPolyline();
+        canvasLayer.drawPolyline(polyline -> Notification.show("Drawn polyline"));
     }
 
     public void drawPolygon() {
-        canvasLayer.drawPolygon();
+        PolygonStyle style = new PolygonStyle();
+        style.setFillColor("indigo");
+        canvasLayer.drawPolygon(polygon -> polygon.setStyle(style));
     }
 
 }
